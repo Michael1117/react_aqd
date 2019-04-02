d53be5474426f6505ffb845b7d76fa66391db5f6
Michael1117 <1322720377@qq.com>
2019-04-02 21:41:30 +0800

    JSX

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
-import React , {Component} from 'react';
+import React from 'react';
 
-class App extends Component{
-    constructor(props) {
-        super(props);
+class App extends React.Component {
+
+    haha(lanmu) {
+        if (lanmu === '篮球') {
+            return (
+                <ul>
+                    <li>姚明</li>
+                    <li>乔丹</li>
+                    <li>科比</li>
+                    <li>奥尼尔</li>
+                </ul>
+            )
+        } else if (lanmu === '足球') {
+            return (
+                <ul>
+                    <li>李铁</li>
+                    <li>郝东海</li>
+                    <li>C罗</li>
+                </ul>
+            )
+        }
     }
 
     render() {
-        return <h1>我是React, 很高兴见到你！ 说{5000+5000}次我爱你！</h1>
+        return (
+            <div>
+                <h1>我是React，很高兴见到你！</h1>
+                <h3>JSX语法很好用哦！ </h3>
+
+                <h3>JSX可以调用函数哦</h3>
+                {3 > 8 ? "A" : "B"}
+                {this.haha('篮球')}
+                <h3>JSX还可以设置样式！</h3>
+                <div style={{width: "100px",height: 20+30+"px", backgroundColor: "red"}}>
+                </div>
+            </div>
+        )
     }
 }
 
-export default App
+export default App;
--- src/main.js
+++ src/main.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import ReactDom from 'react-dom';
-//import {render} from 'react-dom'
+//import ReactDom from 'react-dom';
+import {render} from 'react-dom'
 import App from './App.js'
 
 
+/*
 ReactDom.render(
     <App></App>,
     document.querySelector('#root')
 )
+*/
 
+render(
+    <div>
+        <App></App>
+    </div>,
+    document.querySelector('#root')
+)
