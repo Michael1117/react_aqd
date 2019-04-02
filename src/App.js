11505f01efb97efc6a978dcbe18972353b23b76b
Michael1117 <1322720377@qq.com>
2019-04-02 21:26:31 +0800

    react-init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React , {Component} from 'react';
+
+class App extends Component{
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <h1>我是React, 很高兴见到你！ 说{5000+5000}次我爱你！</h1>
+    }
+}
+
+export default App
